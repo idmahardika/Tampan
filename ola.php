ec10c2245c8c345ae1824fd871ede2a90bb22c46
idmahardika <id.mahardika@gmail.com>
2015-02-02 20:24:52 +0700

    Update ola.php

--- ola.php
+++ ola.php
@@ -1,9 +1,12 @@
 <?php
+/* Paste This File to 
+wp-content/plugins/paid-memberships-pro/adminpages/
+*/
 $servername = "localhost";
 $username = "YOURDBUSERNAME";
 $password = "YOURDBPASS";
 $dbname = "YOUTDBNAME";
-
+//Anyway, this is hardcode. Can you make it as WP function?
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
@@ -17,6 +20,7 @@ if(isset($_REQUEST['ola']))
 $sql = "UPDATE wp_pmpro_memberships_users SET status='$ola' WHERE user_id ='$uid'";
 
 if (mysqli_query($conn, $sql)) {
+	//i'm too lazy call WP reference, so please change the domain with your
 	header("Location: http://kriuq.com/wp-admin/admin.php?l=&page=pmpro-memberslist");
 die();
 } else {
